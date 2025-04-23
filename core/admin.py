3a5047d100d33fde6d0e8856c0f1ac2d20d26b95
ianpelentir <ianpelentir7@gmail.com>
2025-04-23 14:57:01 -0400

    banco de dados esta começando a funcionar

--- core/admin.py
+++ core/admin.py
@@ -1,128 +1,49 @@
 from django.contrib import admin
 from .models import (
-    CoreAuthUser,
-    CoreAuthGroup,
-    CoreAuthPermission,
-    CoreAuthUserGroups,
-    CoreAuthUserUserPermissions,
-    CoreAmbulatorio,
-    CoreAtende,
-    CoreConsulta,
-    CoreConvenio,
-    CoreMedico,
-    CorePaciente,
-    CorePossui,
-    DjangoAdminLog,
-    DjangoContentType,
-    DjangoMigrations,
+    Ambulatorio,
+    Paciente,
+    Medico,
+    Convenio,
+    Consulta,
+    Atende,
+    Possui,
 )
 
-
-
-@admin.register(CoreAuthUser)
-class CoreAuthUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_active')
-    search_fields = ('username', 'email')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('username',)
-
-
-@admin.register(CoreAuthGroup)
-class CoreAuthGroupAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-    ordering = ('name',)
-
-
-@admin.register(CoreAuthPermission)
-class CoreAuthPermissionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'content_type', 'codename')
-    search_fields = ('name', 'content_type__app_label')
-    ordering = ('name',)
-
-
-@admin.register(CoreAuthUserGroups)
-class CoreAuthUserGroupsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'group')
-    search_fields = ('user__username', 'group__name')
-    ordering = ('user',)
-
-
-@admin.register(CoreAuthUserUserPermissions)
-class CoreAuthUserUserPermissionsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'permission')
-    search_fields = ('user__username', 'permission__name')
-    ordering = ('user',)
-
-
-@admin.register(CoreAmbulatorio)
-class CoreAmbulatorioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'numleitos', 'andar')
+@admin.register(Ambulatorio)
+class AmbulatorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'numleitos', 'andar')
     search_fields = ('nome',)
-    ordering = ('nome',)
 
-
-@admin.register(CoreMedico)
-class CoreMedicoAdmin(admin.ModelAdmin):
-    list_display = ('crm', 'nome', 'especialidade', 'idade', 'salario')
-    search_fields = ('nome', 'especialidade')
-    ordering = ('nome',)
-
-
-@admin.register(CorePaciente)
-class CorePacienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cidade', 'idade')
+@admin.register(Paciente)
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'cidade', 'idade', 'ambulatorio')
     search_fields = ('nome', 'cidade')
-    ordering = ('nome',)
+    list_filter = ('cidade',)
 
+@admin.register(Medico)
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ('crm', 'nome', 'especialidade', 'idade', 'salario', 'ambulatorio')
+    search_fields = ('nome', 'especialidade')
+    list_filter = ('especialidade',)
 
-@admin.register(CoreConvenio)
-class CoreConvenioAdmin(admin.ModelAdmin):
+@admin.register(Convenio)
+class ConvenioAdmin(admin.ModelAdmin):
     list_display = ('codconv', 'nome')
     search_fields = ('nome',)
-    ordering = ('nome',)
 
+@admin.register(Consulta)
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'data', 'horario', 'medico', 'paciente', 'convenio', 'porcent')
+    list_filter = ('data', 'convenio')
+    search_fields = ('medico__nome', 'paciente__nome')
 
-@admin.register(CoreConsulta)
-class CoreConsultaAdmin(admin.ModelAdmin):
-    list_display = ('data', 'horario', 'porcent')
-    list_filter = ('data',)
-    ordering = ('-data',)
-
-
-@admin.register(CoreAtende)
-class CoreAtendeAdmin(admin.ModelAdmin):
+@admin.register(Atende)
+class AtendeAdmin(admin.ModelAdmin):
     list_display = ('medico', 'convenio')
     search_fields = ('medico__nome', 'convenio__nome')
-    ordering = ('medico',)
 
-
-@admin.register(CorePossui)
-class CorePossuiAdmin(admin.ModelAdmin):
+@admin.register(Possui)
+class PossuiAdmin(admin.ModelAdmin):
     list_display = ('paciente', 'convenio', 'tipo', 'vencimento')
+    list_filter = ('tipo', 'vencimento')
     search_fields = ('paciente__nome', 'convenio__nome')
-    ordering = ('paciente',)
-
-
-@admin.register(DjangoAdminLog)
-class DjangoAdminLogAdmin(admin.ModelAdmin):
-    list_display = ('action_time', 'user', 'content_type', 'object_id', 'action_flag')
-    search_fields = ('user__username',)
-    ordering = ('-action_time',)
-
-
-@admin.register(DjangoContentType)
-class DjangoContentTypeAdmin(admin.ModelAdmin):
-    list_display = ('app_label', 'model')
-    search_fields = ('app_label', 'model')
-    ordering = ('app_label',)
-
-
-@admin.register(DjangoMigrations)
-class DjangoMigrationsAdmin(admin.ModelAdmin):
-    list_display = ('app', 'name', 'applied')
-    search_fields = ('app', 'name')
-    ordering = ('-applied',)
-
-
-
--- core/models.py
+++ core/models.py
@@ -1,186 +1,117 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
-
 from django.db import models
-from django.contrib.auth.models import User, Group, Permission
 
-class CoreAuthGroup(Group):
-    class Meta:
-        db_table = 'core_auth_group'
-
-    def __str__(self):
-        return self.name
-
-class CoreAuthUser(User):
-    class Meta:
-        db_table = 'core_auth_user'
-
-    def __str__(self):
-        return self.username
-
-class CoreAuthPermission(Permission):
-    class Meta:
-        db_table = 'core_auth_permission'
-
-    def __str__(self):
-        return self.name
-
-class CoreAuthUserGroups(models.Model):
-    user = models.ForeignKey(CoreAuthUser, on_delete=models.CASCADE)
-    group = models.ForeignKey(CoreAuthGroup, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'core_auth_user_groups'
-
-    def __str__(self):
-        return f'{self.user.username} - {self.group.name}'
-
-class CoreAuthGroupPermissions(models.Model):
-    group = models.ForeignKey(CoreAuthGroup, on_delete=models.CASCADE)
-    permission = models.ForeignKey(CoreAuthPermission, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'core_auth_group_permissions'
-
-    def __str__(self):
-        return f'{self.group.name} - {self.permission.name}'
-
-class CoreAuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(CoreAuthUser, on_delete=models.CASCADE)
-    permission = models.ForeignKey(CoreAuthPermission, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'core_auth_user_user_permissions'
-
-    def __str__(self):
-        return f'{self.user.username} - {self.permission.name}'
-
-class CoreAmbulatorio(models.Model):
-    nome = models.CharField(max_length=100)
+class Ambulatorio(models.Model):
+    nome = models.CharField(max_length=200)
     numleitos = models.IntegerField()
     andar = models.IntegerField()
 
     class Meta:
-        db_table = 'core_ambulatorio'
+        db_table = 'ambulatorio'
+        managed = False
+        verbose_name = 'Ambulatório'
+        verbose_name_plural = 'Ambulatórios'
 
     def __str__(self):
         return f'{self.nome} - {self.numleitos} leitos'
 
-class CoreMedico(models.Model):
-    crm = models.CharField(max_length=15)
-    nome = models.CharField(max_length=100)
-    especialidade = models.CharField(max_length=100)
-    idade = models.IntegerField()
-    salario = models.DecimalField(max_digits=10, decimal_places=2)
-    ambulatorio = models.ForeignKey(CoreAmbulatorio, on_delete=models.CASCADE)
 
-    class Meta:
-        db_table = 'core_medico'
-
-    def __str__(self):
-        return f'{self.nome} ({self.crm}) - {self.especialidade}'
-
-class CorePaciente(models.Model):
-    nome = models.CharField(max_length=100)
-    endereco = models.CharField(max_length=255)
-    telefone = models.CharField(max_length=20, null=True, blank=True)
+class Paciente(models.Model):
+    nome = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=250)
+    telefone = models.CharField(max_length=15, null=True, blank=True)
     cidade = models.CharField(max_length=100)
     idade = models.IntegerField()
-    ambulatorio = models.ForeignKey(CoreAmbulatorio, on_delete=models.CASCADE)
+    ambulatorio = models.ForeignKey(Ambulatorio, on_delete=models.CASCADE, related_name='pacientes')
 
     class Meta:
-        db_table = 'core_paciente'
+        db_table = 'paciente'
+        managed = False
+        verbose_name = 'Paciente'
+        verbose_name_plural = 'Pacientes'
 
     def __str__(self):
         return f'{self.nome} - {self.cidade}'
 
-class CoreConvenio(models.Model):
-    codconv = models.IntegerField()
-    nome = models.CharField(max_length=100)
+
+class Medico(models.Model):
+    crm = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=200)
+    especialidade = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=250)
+    telefone = models.CharField(max_length=15, null=True, blank=True)
+    idade = models.IntegerField()
+    salario = models.DecimalField(max_digits=12, decimal_places=2)
+    ambulatorio = models.ForeignKey(Ambulatorio, on_delete=models.CASCADE, related_name='medicos')
 
     class Meta:
-        db_table = 'core_convenio'
+        db_table = 'medico'
+        managed = False
+        verbose_name = 'Médico'
+        verbose_name_plural = 'Médicos'
+
+    def __str__(self):
+        return f'Dr(a). {self.nome} - {self.especialidade} ({self.crm})'
+
+
+class Convenio(models.Model):
+    codconv = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=200)
+
+    class Meta:
+        db_table = 'convenio'
+        managed = False
+        verbose_name = 'Convênio'
+        verbose_name_plural = 'Convênios'
 
     def __str__(self):
         return f'{self.nome} ({self.codconv})'
 
-class CoreConsulta(models.Model):
+
+class Consulta(models.Model):
     data = models.DateField()
     horario = models.TimeField()
-    medico = models.ForeignKey(CoreMedico, on_delete=models.CASCADE)
-    paciente = models.ForeignKey(CorePaciente, on_delete=models.CASCADE)
-    convenio = models.ForeignKey(CoreConvenio, on_delete=models.CASCADE)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    convenio = models.ForeignKey(Convenio, on_delete=models.CASCADE)
     porcent = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
-        db_table = 'core_consulta'
+        db_table = 'consulta'
+        managed = False
+        verbose_name = 'Consulta'
+        verbose_name_plural = 'Consultas'
 
     def __str__(self):
-        return f'Consulta de {self.paciente.nome} com Dr(a). {self.medico.nome} em {self.data}'
+        return f"Consulta de {self.paciente.nome} com {self.medico.nome} em {self.data}"
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    user = models.ForeignKey(CoreAuthUser, on_delete=models.CASCADE)
-    content_type = models.ForeignKey('contenttypes.ContentType', on_delete=models.CASCADE)
-    object_id = models.TextField()
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
+
+class Atende(models.Model):
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    convenio = models.ForeignKey(Convenio, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'core_django_admin_log'
+        db_table = 'atende'
+        unique_together = ('medico', 'convenio')
+        managed = False
+        verbose_name = 'Atende'
+        verbose_name_plural = 'Atende'
 
     def __str__(self):
-        return f'{self.action_time} - {self.user.username}'
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'core_django_content_type'
-
-    def __str__(self):
-        return f'{self.app_label} - {self.model}'
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    applied = models.DateTimeField()
-
-    class Meta:
-        db_table = 'core_django_migrations'
-
-    def __str__(self):
-        return f'{self.app} - {self.name} - {self.applied}'
+        return f"{self.medico.nome} atende o convenio {self.convenio.nome}"
 
 
-
-
-class CoreAtende(models.Model):
-    medico = models.ForeignKey('CoreMedico', on_delete=models.CASCADE)
-    convenio = models.ForeignKey('CoreConvenio', on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'core_atende'
-
-    def __str__(self):
-        return f'{self.medico} atende {self.convenio}'
-
-
-class CorePossui(models.Model):
-    paciente = models.ForeignKey('CorePaciente', on_delete=models.CASCADE)
-    convenio = models.ForeignKey('CoreConvenio', on_delete=models.CASCADE)
+class Possui(models.Model):
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    convenio = models.ForeignKey(Convenio, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=1)
     vencimento = models.DateField()
 
     class Meta:
-        db_table = 'core_possui'
+        db_table = 'possui'
+        unique_together = ('paciente', 'convenio')
+        managed = False
+        verbose_name = 'Possui'
+        verbose_name_plural = 'Possui'
 
     def __str__(self):
-        return f'{self.paciente} possui o convênio {self.convenio}'
+        return f"{self.paciente.nome} possui o convênio {self.convenio.nome}"
